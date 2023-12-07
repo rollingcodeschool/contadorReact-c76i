043218fad37ca9543d760eef50b44c249cab1b3c
Emilse Arias <emilsearias@gmail.com>
2023-12-07 18:27:19 -0300

    agregamos nuestro primer componente Informacion

---
+++ src/components/Informacion.jsx
@@ -0,0 +1,25 @@
+const Informacion = () => {
+  //aqui va la logica del componente
+
+  return (
+    //aqui puede ir un poco de logica
+    <>
+      <h2>Conceptos de react</h2>
+      <section>
+        {/* aqui va todo el maquetado del componente */}
+        <p>
+          React es una biblioteca de JavaScript desarrollada por Facebook que se
+          utiliza para construir interfaces de usuario interactivas y
+          eficientes. Su enfoque se centra en la creación de componentes
+          reutilizables que representan diferentes partes de la interfaz de
+          usuario. React utiliza un paradigma de programación declarativa, lo
+          que significa que los desarrolladores describen cómo debería ser la
+          interfaz de usuario y React se encarga de manejar los cambios de
+          manera eficiente.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Informacion;
