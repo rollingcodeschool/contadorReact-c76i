a4a02204c9c7147a6617229affef2d673f3bb95f
Emilse Arias <emilsearias@gmail.com>
2023-12-12 18:36:19 -0300

    agregamos un state número en el componente contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 const Contador = () => {
-    return (
-        <article className="text-center">
-            <h2>Contador</h2>
-            <p>10</p>
-            <button className="btn btn-primary">+1</button>
-        </article>
-    );
+const [numero, setNumero] = useState(10)
+//   let numero = 10;
+
+  return (
+    <article className="text-center">
+      <h2>Contador</h2>
+      <p>{numero}</p>
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          numero++;
+          console.log(numero);
+        }}
+      >
+        +1
+      </button>
+    </article>
+  );
 };
 
 export default Contador;
